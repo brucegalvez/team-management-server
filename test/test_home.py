f27fa5e730b9311b9f0548f27b01ae2d0de92344
Javier Cardenas <javiercs893@gmail.com>
2020-04-30 00:28:21 -0500

    Corrige archivo test_home
    - Elimina codigo duplicado

--- test/test_home.py
+++ test/test_home.py
@@ -15,11 +15,3 @@ class HomeTest(unittest.TestCase):
     def test_loginUser(self):
         response = self.app.post(LOGIN_URL)
         self.assertEqual(201, response.status_code)
-
-    def test_UserDashboard(self):
-        response = self.app.get(FRIENDS_URL)
-        self.assertEqual(200, response.status_code)
-
-    def test_UserInfo(self):
-        response = self.app.get(FRIEND_URL)
-        self.assertEqual(200, response.status_code)
