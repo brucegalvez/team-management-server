119d82a5e3e4cf77c4c6e13b1fe9cb967029a965
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2020-04-30 01:09:19 -0500

    Valida y controla el recurso de crear usuarios

--- controllers/homeController.py
+++ controllers/homeController.py
@@ -4,15 +4,29 @@
 # Módulos propios
 from server import *
 from logger import *
+from utils.connection import Connection
+from flask_bcrypt import generate_password_hash, check_password_hash
+
 
 # Librerias importadas
 from flask_restx import Resource
+from flask import request
+
+connection = Connection()
 
 
 class UserCreator(Resource):
     # Ruta para crear un nuevo usuario
     def post(self):
-        return 'Bienvenido nuevo usuario', 201
+        user = request.get_json()
+        requiredFields = ['firstName', 'lastName', 'email', 'password']
+        if all(fields in requiredFields for fields in user):
+            user['password'] = generate_password_hash(
+                user['password']).decode('utf8')
+            connection.database.users.insert_one(user)
+            return 'Contacto anadido', 201
+        else:
+            return 'Missing fields', 400
 
 
 class LoginController(Resource):
