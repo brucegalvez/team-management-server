98935ed3c9e60d9ff42542e83f766e23a8a102e9
Javier Cardenas <javiercs893@gmail.com>
2020-04-29 23:28:30 -0500

    Agrega ultima conexion a respuesta
    - Habilita ultima conexion
    - Mejora respuesta de put de estado de conexion

--- controllers/profileController.py
+++ controllers/profileController.py
@@ -4,6 +4,7 @@
 # Módulos propios
 from server import *
 from logger import *
+from lastConection import *
 
 # Librerias importadas
 from flask_restx import Resource
@@ -13,15 +14,25 @@ from flask import jsonify, request
 class UserStatus(Resource):
     # Ruta para cambiar tu estado de conexion
     def put(self):
+        
+        # Declaramos la lista de valores posibles
         estados = ['Conectado', 'Ocupado', 'Ausente',
                    'No Disponible', 'Desconectado']
+        
+        # Capturamos la respuesta en una variable
         data = request.get_json()
         nuevo_estado = data["estado de conexion"]
-        print(nuevo_estado)
+        
+        # Validamos que el estado esté en la lista
         if nuevo_estado in estados:
-            return {"message": f'Tu nuevo estado de conexion es: {nuevo_estado}', "success": "true"}, 200
+            return {
+                'message': f'Tu nuevo estado de conexion es: {nuevo_estado}',
+                'connection': f'Ultima conexion: {last_conection}',
+                "success": "true"}, 200
         else:
-            return {"message": "Elija una opción correcta", "success": "false"}, 200
+            return {
+                'message': 'Elija una opción correcta',
+                'success': 'false'}, 200
 
 
 class UserProfileData(Resource):
---
+++ lastConection.py
@@ -0,0 +1,6 @@
+# Importamos modulos para capturar el tiempo
+import datetime
+
+# Se utilizara para obtener la ultima conexión del usuario
+x = datetime.datetime.now()
+last_conection = x.strftime("%Y-%B-%A %I:%M:%S %p")
