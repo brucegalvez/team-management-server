181487c8ebc31d8d90ae381c8aa15768265db676
Javier Cardenas <javiercs893@gmail.com>
2020-05-01 03:30:35 -0500

    Completa PUT de estatus en Mongo
    - Valida informacion
    - Refactoriza metodo put y get de usuario
    - Habilita username key en metodo post
    - Modifica URL de peticion

---
+++ controllers/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Módulos propios
+from server import *
+from logger import *
+from utils.lastConnection import *
+from utils.connection import Connection
+from utils.environment import Environment
+
+
+# Librerias importadas
+import datetime
+from flask import request, jsonify
+from flask_restx import Resource
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+
+connection = Connection()
--- utils/connection.py
+++ utils/connection.py
@@ -7,3 +7,16 @@ class Connection():
         config = Environment().settingsDB()
         client = MongoClient(config['DB_HOST'], config['DB_PORT'])
         self.database = client[config['DB_DATABASE']]
+        # self.users = self.database.users
+
+    # Metodo de prueba para sacar un registro mediante username
+    def mostrarRegistro(self, username):
+        return self.database.users.find_one(
+            {"username": f'{username}'})
+
+    # Metodo para actualizar un (1) usuario.
+    # Este recibe el usuario a actualizar como user
+    # y el valor a cambiar como newValue
+    def updateUserData(self, user, newValue):
+        return self.database.users.update_one(user, newValue)
+        logging.info(x.modified_count, "Dato actualizado")
