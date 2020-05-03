d3155aeb51a369b4e22c6f829bfb0a2f07a9de9e
Javier Cardenas <javiercs893@gmail.com>
2020-05-03 17:22:59 -0500

    Actualiza tests de put
    - Actualiza put status
    - Actualiza put profile data

--- test/test_profile.py
+++ test/test_profile.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from . import *
+from .test_home import HomeTest
 
 
 class ProfileTest(unittest.TestCase):
     def setUp(self):
         app.config['DEBUG'] = False
+        app.config['MONGO_URI'] = 'mongodb://localhost:27017/intranet'
         self.app = app.test_client()
+        self.app.post(
+            SIGNUP_URL,
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(HomeTest().generateUser(1))
+        )
 
-    def test_ProfileData(self):
-        response = self.app.put(PROFILE_URL)
-        self.assertEqual(204, response.status_code)
+    def test_UserProfileData(self):
+
+        payload = json.dumps({
+            "profile": "Estudio en PachaQtec",
+            "program": "FrontEnd",
+            "campus": "Lima",
+            "phone": "+51991467953",
+            "email": "aho123@mail.com",
+            "campus": "Ate",
+            "program": "BackEnd"
+        })
+
+        response = self.requests.put(PROFILE_URL,
+                                     headers={
+                                         "Content-Type": "application/json"},
+                                     data=payload)
+
+        data = response.json()
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("true", data['success'])
 
     def test_UserStatus(self):
         payload = json.dumps({
@@ -26,4 +50,3 @@ class ProfileTest(unittest.TestCase):
         data = response.json()
         self.assertEqual(200, response.status_code)
         self.assertEqual("true", data["success"])
-        self.assertIsNotNone(payload["status"])
