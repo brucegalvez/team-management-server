fb4d0a7401a99dd85f4baa611bdc88bcc1e0320f
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2020-05-03 13:44:23 -0500

    Refactoriza los controllers signin y login
    - Utiliza conexion flask_pymongo
    - Valida los campos enviados
    - Crea a los usuarios en status Conectados
    - Simplifica los responses

--- controllers/homeController.py
+++ controllers/homeController.py
@@ -9,38 +9,53 @@ class UserCreator(Resource):
     def post(self):
         user = request.get_json()
         requiredFields = ['firstName', 'lastName',
-                          'username', 'email', 'password', 'status']
-        if not all(fields in requiredFields for fields in user):
+                          'username', 'email', 'password']
+        if list(user.keys()) != requiredFields:
             return {
                 'message': 'Error en los campos enviados.',
                 'success': 'false'}, 200
+        elif not (
+            validate.validateGivenName(user['firstName']) and
+            validate.validateGivenName(user['lastName']) and
+            validate.validateUsername(user['username']) and
+            validate.validateEmail(user['email']) and
+            validate.validatePassword(user['password'])
+        ):
+            return {
+                'message': 'Error en los campos enviados.',
+                'success': 'false'}, 200
+        elif list(mongo.db.users.find(
+                {"$or": [{'email': user['email']}, {'username': user['username']}]})):
+            return {
+                'message': 'Existe un usuario con el mismo email o username.',
+                'success': 'false'}, 200
         else:
-            if connection.database.users.find_one({'email': user['email']}):
-                return {
-                    'message': 'Un usuario con el mismo email ya existe.',
-                    'success': 'false'}, 200
-            else:
-                user['password'] = generate_password_hash(
-                    user['password']).decode('utf8')
-                connection.database.users.insert_one(user)
-                return {
-                    'message': 'Bienvenido a Pachaqtec. Cuenta creada con éxito.',
-                    'success': 'true'}, 200
+            user['password'] = generate_password_hash(
+                user['password']).decode('utf8')
+            user['status'] = 'Conectado'
+            mongo.db.users.insert_one(user)
+            return {
+                'message': 'Cuenta creada con éxito.',
+                'success': 'true'}, 200
 
 
 class LoginController(Resource):
-
     # Ruta para la API de validar la contraseña con Json Token
     def post(self):
         user = request.get_json()
         requiredFields = ['email', 'password']
-        if not all(fields in requiredFields for fields in user):
+        if list(user.keys()) != requiredFields:
+            return {
+                'message': 'Email o contraseña incorrectas',
+                'success': 'false'}, 200
+        elif not (
+                validate.validateEmail(user['email']) and
+                validate.validatePassword(user['password'])):
             return {
                 'message': 'Email o contraseña incorrectas',
                 'success': 'false'}, 200
         else:
-            foundUser = connection.database.users.find_one(
-                {'email': user['email']})
+            foundUser = mongo.db.users.find_one({'email': user['email']})
             if not check_password_hash(foundUser["password"], user['password']):
                 return {
                     'message': 'Email o contraseña incorrectas',
@@ -50,6 +65,7 @@ class LoginController(Resource):
                 access_token = create_access_token(
                     identity=foundUser['username'], expires_delta=expires)
                 return {
+                    'success': 'true',
                     'username': foundUser['username'],
                     'token': access_token
                 }, 200
