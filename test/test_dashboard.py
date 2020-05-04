ca745f44471df9b3035e217048b333f8cb73add5
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2020-05-04 13:33:29 -0500

    Testea el recurso UserInfo

--- test/test_dashboard.py
+++ test/test_dashboard.py
@@ -3,15 +3,46 @@
 from . import *
 
 
-class ProfileTest(unittest.TestCase):
+class DashboardTest(unittest.TestCase):
     def setUp(self):
         app.config['DEBUG'] = False
+        app.config['MONGO_URI'] = 'mongodb://localhost:27017/intranet'
         self.app = app.test_client()
+        # Creo dos usuarios en la DB y obtengo un token para el primero
+        signUpKeys = ["firstName", "lastName",
+                      "username", "email", "password"]
+        self.app.post(
+            SIGNUP_URL,
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(generateUser(idNum=1, filterKeys=signUpKeys)))
+        self.app.post(
+            SIGNUP_URL,
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(generateUser(idNum=2, filterKeys=signUpKeys)))
+        response = self.app.post(
+            LOGIN_URL,
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(generateUser(
+                idNum=1, filterKeys=["email", "password"])))
+        data = json.loads(response.data)
+        self.token = data['content']['token']
 
-    def test_UserDashboard(self):
-        response = self.app.get(FRIENDS_URL)
+    def test_userInfo_success(self):
+        response = self.app.get(
+            f"{FRIENDS_URL}/testUsername2",
+            headers={"Content-Type": "application/json",
+                     "Authorization": f"Bearer {self.token}"})
+        data = json.loads(response.data)
         self.assertEqual(200, response.status_code)
+        self.assertEqual("true", data['success'])
 
-    def test_UserInfo(self):
-        response = self.app.get(FRIEND_URL)
+    def test_userInfo_error(self):
+        response = self.app.get(
+            f"{FRIENDS_URL}/testUsername2",
+            headers={"Content-Type": "application/json"})
+        data = json.loads(response.data)
         self.assertEqual(200, response.status_code)
+        self.assertEqual("false", data['success'])
+
+    def tearDown(self):
+        return super().tearDown()
