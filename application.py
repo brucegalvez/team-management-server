ee635e7ce996326795c51a9b69703025ce338d49
Javier Cardenas <javiercs893@gmail.com>
2020-05-06 00:17:46 -0500

    Crea recurso PUT tags
    - Implementa actualizacion de tags
    - Aprueba tests con exito

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from server import *
 from logger import *
 from controllers.homeController import UserCreator, LoginController
 from controllers.dashboardController import UsersDashboard, UserInfo
-from controllers.profileController import UserProfileData
+from controllers.profileController import UserProfileData, UserTags
 from controllers.chatController import UserStatus, ChatDisplay
 
 
@@ -17,16 +17,18 @@ home = api.namespace('home', description='Sign up & Login API')
 home.add_resource(UserCreator, '/signup')
 home.add_resource(LoginController, '/login')
 
-# La siguiente api, recibe peticiones dentro del chat
-chatroom = api.namespace('chat', description='Chat API')
-chatroom.add_resource(UserStatus, '/<username>')
-chatroom.add_resource(ChatDisplay, '')
 
 # Las siguientes funcionan se accionan desde un dashboard
 dashboard = api.namespace('dashboard', description='Dashboard API')
 dashboard.add_resource(UsersDashboard, '/friends')
 dashboard.add_resource(UserInfo, '/friends/<username>')
 dashboard.add_resource(UserProfileData, '/<username>/profile')
+dashboard.add_resource(UserTags, '/<username>/profile/tags')
+
+# La siguiente api, recibe peticiones dentro del chat
+chatroom = api.namespace('chat', description='Chat API')
+chatroom.add_resource(UserStatus, '/<username>')
+chatroom.add_resource(ChatDisplay, '')
 
 if __name__ == '__main__':
     config = Environment().settingsGeneral()
