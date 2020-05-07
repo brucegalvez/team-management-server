3f58dd6949a9e74ab03d6647512772381acc0723
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2020-05-06 20:53:45 -0500

    Valida birthdays y campus
    - Elimina data sensible grabada al documento .log

--- utils/validate.py
+++ utils/validate.py
@@ -1,5 +1,6 @@
 import re
 from logger import *
+import datetime
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -12,10 +13,10 @@ class Validate():
         rule = re.compile('[+]519\d{8}$')
 
         if re.match(rule, phone):
-            logging.debug(f"Número validado: {phone}")
+            logging.debug("Número validado:")
             return True
         else:
-            logging.debug(f'{phone} no es un número correcto')
+            logging.debug('No es un número correcto')
             return False
 
     # El correo que ingresa por parametro debe coincidir
@@ -24,45 +25,63 @@ class Validate():
         rule = re.compile('[a-zA-z0-9_.+-]+@[a-zA-Z-]+\.[a-z]+$')
 
         if re.match(rule, email):
-            logging.debug(f"Correo validado: {email}")
+            logging.debug("Correo validado")
             return True
         else:
-            logging.debug(f'{email} no es un email correcto')
+            logging.debug('No es un email correcto')
             return False
 
     def validatePassword(self, password):
         rule = re.compile('[A-Za-z0-9@#$%^&+=]{8,}')
         if re.match(rule, password):
-            logging.debug(f"Contraseña validado: {password}")
+            logging.debug("Contraseña validado")
             return True
         else:
-            logging.debug(f'{password} no es una contraseña válida')
+            logging.debug('No es una contraseña válida')
             return False
 
     def validateGivenName(self, givenName):
         rule = re.compile('[A-Za-z]{,45}')
         if re.match(rule, givenName):
-            logging.debug(f"Nombre validado: {givenName}")
+            logging.debug("Nombre validado")
             return True
         else:
-            logging.debug(f'{givenName} no es un Nombre válido')
+            logging.debug('No es un Nombre válido')
             return False
 
     def validateUsername(self, username):
         rule = re.compile('[A-Za-z0-9]{,30}')
         if re.match(rule, username):
-            logging.debug(f"Usuario validado: {username}")
+            logging.debug("Usuario validado")
             return True
         else:
-            logging.debug(f'{username} no es un usuario válido')
+            logging.debug('No es un usuario válido')
             return False
 
     def validateProfile(self, profile):
         rule = re.compile("[^']{,150}$")
 
         if re.match(rule, profile):
-            logging.debug(f"Perfil validado: {profile}")
+            logging.debug("Perfil validado")
             return True
         else:
-            logging.debug(f'{profile} tiene más de 150 caracteres')
+            logging.debug('Tiene más de 150 caracteres')
+            return False
+
+    def validateCampus(self, campus):
+        validCampus = ['Lima Centro', 'Wilson', 'San Juan de Miraflores',
+                       'San Juan de Lurigancho', 'Tomas Valle - Lima Norte', 'Ate - Lima Este']
+        if campus in validCampus:
+            logging.debug("Campus validado")
+            return True
+        else:
+            logging.debug('El campus usado no existe')
+            return False
+
+    def validateBirthday(self, birthday):
+        if type(birthday) is datetime.datetime:
+            logging.debug("La fecha de cumpleaños ha sido validada")
+            return True
+        else:
+            logging.debug('La fecha no es válida')
             return False
